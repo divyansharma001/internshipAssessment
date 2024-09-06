3550cc66b3cdb72818ca1d6dda018cb1a74f39e9
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-09-06 19:37:14 +0530

    Complete Frontpage

---
+++ Client/src/Pages/FrontPage/FrontPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const FrontPage = () => {
+  return (
+    <div className="w-full bg-[#FEFFDE] h-screen flex items-center justify-center">
+  <div className="text-center">
+    <div className="text-4xl font-bold">
+      Welcome!
+    </div>
+    <div className="mt-4 text-lg">
+      Your feedback is invaluable. Please let us know your thoughts.
+    </div>
+
+    <button className="mt-6 bg-[#FFD700] px-4 py-2 rounded-lg text-lg">Start</button>
+  </div>
+</div>
+
+  );
+};
+
+export default FrontPage;
